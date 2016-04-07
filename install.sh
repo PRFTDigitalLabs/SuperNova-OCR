97fdbe6349a4ebef16167edb16ce9c0b955159e9
dt-tl <dan@truthlabs.com>
2016-04-06 22:28:29 -0500

    updated install script
    If you have node / npm problems… sorry :(

--- install.sh
+++ install.sh
@@ -1,20 +1,53 @@
 #!/bin/sh
 xcode-select --install
+wait $!
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/science
-brew install homebrew/science/opencv
+\curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enabled
+wait $1
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+wait $1
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+  </dev/null
+wait $!
 
-brew install pkg-config cairo libpng jpeg giflib imlib2
+echo 😢 😢 😢 This takes FOREVER! 😢 😢 😢
 
 brew cask install xquartz --force
-ln -s /opt/X11/include/X11 /usr/local/include/X11
+wait $!
 
-curl https://www.unix-ag.uni-kl.de/~auerswal/ssocr/ssocr-2.16.3.tar.bz2
+sudo ln -s /opt/X11/include/X11 /usr/local/include/X11
+wait $!
+
+brew tap homebrew/science
+wait $!
+
+brew install homebrew/science/opencv
+wait $!
+
+brew install pkg-config cairo libpng jpeg giflib imlib2
+wait $!
+
+curl -O https://www.unix-ag.uni-kl.de/~auerswal/ssocr/ssocr-2.16.3.tar.bz2
+wait $!
 bzip2 -dc ssocr-2.16.3.tar.bz2 | tar -xvf -
+wait $!
 cd ssocr-2.16.3
 sudo make install
+wait $!
+
+rm ssocr-2.16.3.tar.bz2
+wait $!
+rm -rf ssocr-2.16.3
+wait$!
 
 brew install node
+wait $!
+sudo npm update -g
+wait $!
+npm install -g node-gyp
+wait $!
+brew reinstall pkg-config
+wait $!
 sudo npm install
+wait $!
 node server/install-test.js
